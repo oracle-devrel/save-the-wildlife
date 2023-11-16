1703c1a479e1caab8d3d3fd2de02247a0e36f5c4
Wojciech Pluta <60693402+WSPluta@users.noreply.github.com>
2023-11-16 16:04:44 +0000

    updating webpack.dev for local dev

--- web/bundler/webpack.dev.js
+++ web/bundler/webpack.dev.js
@@ -1,34 +1,53 @@
+const path = require("path");
 const { merge } = require("webpack-merge");
-const commonConfiguration = require("./webpack.common.js");
 const portFinderSync = require("portfinder-sync");
-
-const infoColor = (_message) => {
-  return `\u001b[1m\u001b[34m${_message}\u001b[39m\u001b[22m`;
-};
+const commonConfiguration = require("./webpack.common.js");
 
 module.exports = merge(commonConfiguration, {
   mode: "development",
+
   devServer: {
     host: "localhost",
     port: portFinderSync.getPort(8080),
-    contentBase: "./dist",
-    watchContentBase: true,
+    static: [
+      {
+        directory: path.resolve(__dirname, "dist"),
+        watch: true,
+      },
+      {
+        directory: path.resolve(__dirname, "static"),
+        staticOptions: {},
+        publicPath: "/static-public-path/",
+        serveIndex: true,
+        watch: true,
+      },
+    ],
     proxy: {
-      "/socket.io": { target: "ws://localhost:3000" },
-      "/api": { target: "ws://localhost:8080" },
+      "/socket.io": { target: "http://localhost:3000", ws: true },
+      "/api": { target: "http://localhost:8080", ws: true },
     },
     open: true,
     https: false,
-    useLocalIp: false,
-    disableHostCheck: true,
-    overlay: true,
-    noInfo: true,
-    after: function (app, server, compiler) {
-      const port = server.options.port;
-      const https = server.options.https ? "s" : "";
-      const domain = `http${https}://localhost:${port}`;
-
-      console.log(`Project running at:\n  - ${infoColor(domain)}`);
+    allowedHosts: "all",
+    onAfterSetupMiddleware: function (devServer) {
+      devServer.app.get("/some/path", function (req, res) {
+        res.json({ custom: "response" });
+      });
+    },
+    client: {
+      logging: "info",
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+      progress: true,
+      reconnect: true,
+      webSocketTransport: "ws", 
+      webSocketURL: {
+        hostname: "localhost",
+        pathname: "/ws",
+        port: 8080,
+      },
     },
   },
 });
